8ad7ca788229f503183b5c9a8813371e912cd514
Justin Warner <jkwdives@hotmail.com>
2023-03-24 15:24:06 -0700

    Add wordbreakdp

--- src/General_Questions/StringQuestions.java
+++ src/General_Questions/StringQuestions.java
@@ -796,6 +796,20 @@ public class StringQuestions {
 		noMatch.add(s);
 		return false;
 	}
+	public static boolean wordBreakDP(String s, List<String> wordDict) {
+		Set<String> wordDictSet = new HashSet<>(wordDict);
+		boolean[] dp = new boolean[s.length() + 1];
+		dp[0] = true;
+		for (int i = 1; i <= s.length(); i++) {
+			for (int j = 0; j < i; j++) {
+				if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+		return dp[s.length()];
+	}
 	// Runtime:
 	public static boolean wordBreakPartitioning(String s, List<String> wordDict) {
 		return wordBreakPartitioning(s, new HashSet<>(wordDict), new HashSet<>());
